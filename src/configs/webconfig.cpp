786c4cd152bc58ef65f3d66b15c32c7dd4a1ad67
deeebug <77402236+deeebug@users.noreply.github.com>
2023-05-14 07:26:38 +0545

    Migrate BOOTSEL button addon to ProtoBuf

--- src/configs/webconfig.cpp
+++ src/configs/webconfig.cpp
@@ -808,6 +808,10 @@ std::string setAddonOptions()
 	docToPin(analogOptions.analogAdcPinY, doc, "analogAdcPinY");
 	docToValue(analogOptions.enabled, doc, "AnalogInputEnabled");
 
+    BootselButtonOptions& bootselButtonOptions = Storage::getInstance().getAddonOptions().bootselButtonOptions;
+	docToValue(bootselButtonOptions.buttonMap, doc, "bootselButtonMap");
+	docToValue(bootselButtonOptions.enabled, doc, "BootselButtonAddonEnabled");
+
 	ReverseOptions& reverseOptions = Storage::getInstance().getAddonOptions().reverseOptions;
 	docToValue(reverseOptions.enabled, doc, "ReverseInputEnabled");
 	docToPin(reverseOptions.buttonPin, doc, "reversePin");	
@@ -850,7 +854,6 @@ std::string setAddonOptions()
 	docToPinLegacy(addonOptions.pinDualDirRight, doc, "dualDirRightPin");
 	docToValue(addonOptions.dualDirDpadMode, doc, "dualDirDpadMode");
 	docToValue(addonOptions.dualDirCombineMode, doc, "dualDirCombineMode");
-	docToValue(addonOptions.bootselButtonMap, doc, "bootselButtonMap");
 	docToPinLegacy(addonOptions.buzzerPin, doc, "buzzerPin");
 	docToValue(addonOptions.buzzerVolume, doc, "buzzerVolume");
 	docToPinLegacy(addonOptions.extraButtonPin, doc, "extraButtonPin");
@@ -876,7 +879,6 @@ std::string setAddonOptions()
 	docToValue(addonOptions.wiiExtensionBlock, doc, "wiiExtensionBlock");
 	docToValue(addonOptions.wiiExtensionSpeed, doc, "wiiExtensionSpeed");
 	docToValue(addonOptions.BuzzerSpeakerAddonEnabled, doc, "BuzzerSpeakerAddonEnabled");
-	docToValue(addonOptions.BootselButtonAddonEnabled, doc, "BootselButtonAddonEnabled");
 	docToValue(addonOptions.DualDirectionalInputEnabled, doc, "DualDirectionalInputEnabled");
 	docToValue(addonOptions.ExtraButtonAddonEnabled, doc, "ExtraButtonAddonEnabled");
 	docToValue(addonOptions.I2CAnalog1219InputEnabled, doc, "I2CAnalog1219InputEnabled");
@@ -997,6 +999,10 @@ std::string getAddonOptions()
 	writeDoc(doc, "analogAdcPinY", analogOptions.analogAdcPinY == 0xFF ? -1 : analogOptions.analogAdcPinY);
 	writeDoc(doc, "AnalogInputEnabled", analogOptions.enabled);
 
+    const BootselButtonOptions& bootselButtonOptions = Storage::getInstance().getAddonOptions().bootselButtonOptions;
+	writeDoc(doc, "bootselButtonMap", bootselButtonOptions.buttonMap);
+	writeDoc(doc, "BootselButtonAddonEnabled", bootselButtonOptions.enabled);
+
 	const ReverseOptions& reverseOptions = Storage::getInstance().getAddonOptions().reverseOptions;
 	writeDoc(doc, "reversePin", isValidPin(reverseOptions.buttonPin) ? reverseOptions.buttonPin : -1);
 	writeDoc(doc, "reversePinLED", isValidPin(reverseOptions.ledPin) ? reverseOptions.ledPin : -1);
@@ -1035,7 +1041,6 @@ std::string getAddonOptions()
 	writeDoc(doc, "dualDirRightPin", addonOptions.pinDualDirRight == 0xFF ? -1 : addonOptions.pinDualDirRight);
 	writeDoc(doc, "dualDirDpadMode", addonOptions.dualDirDpadMode);
 	writeDoc(doc, "dualDirCombineMode", addonOptions.dualDirCombineMode);
-	writeDoc(doc, "bootselButtonMap", addonOptions.bootselButtonMap);
 	writeDoc(doc, "buzzerPin", addonOptions.buzzerPin == 0xFF ? -1 : addonOptions.buzzerPin);
 	writeDoc(doc, "buzzerVolume", addonOptions.buzzerVolume);
 	writeDoc(doc, "extraButtonPin", addonOptions.extraButtonPin == 0xFF ? -1 : addonOptions.extraButtonPin);
@@ -1061,7 +1066,6 @@ std::string getAddonOptions()
 	writeDoc(doc, "wiiExtensionBlock", addonOptions.wiiExtensionBlock);
 	writeDoc(doc, "wiiExtensionSpeed", addonOptions.wiiExtensionSpeed);
 	writeDoc(doc, "BuzzerSpeakerAddonEnabled", addonOptions.BuzzerSpeakerAddonEnabled);
-	writeDoc(doc, "BootselButtonAddonEnabled", addonOptions.BootselButtonAddonEnabled);
 	writeDoc(doc, "DualDirectionalInputEnabled", addonOptions.DualDirectionalInputEnabled);
 	writeDoc(doc, "ExtraButtonAddonEnabled", addonOptions.ExtraButtonAddonEnabled);
 	writeDoc(doc, "I2CAnalog1219InputEnabled", addonOptions.I2CAnalog1219InputEnabled);
